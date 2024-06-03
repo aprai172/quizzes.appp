bcafc39405c2d757b65cd689a65149244dabe30c
ayushrai <63505520+ayushrai-bhau@users.noreply.github.com>
2024-06-03 20:22:02 +0530

    link

--- frontend/src/components/ShareLink/PublishModal.jsx
+++ frontend/src/components/ShareLink/PublishModal.jsx
@@ -9,7 +9,7 @@ const PublishModal = () => {
   let id  = localStorage.getItem("quizId")
   console.log(id)
 
-  let link = `http://localhost:3000/test/${id}`
+  let link = `https://quizees.onrender.com/test/${id}`
 
   const handleShareClick = () => {
     navigator.clipboard.writeText(link);
