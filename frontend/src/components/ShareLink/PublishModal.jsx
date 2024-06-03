d4175e0bb9b7128f7619d2ef3937e6346cb2cade
ayushrai <63505520+ayushrai-bhau@users.noreply.github.com>
2024-06-03 12:02:39 +0530

    ink

--- frontend/src/components/ShareLink/PublishModal.jsx
+++ frontend/src/components/ShareLink/PublishModal.jsx
@@ -9,7 +9,7 @@ const PublishModal = () => {
   let id  = localStorage.getItem("quizId")
   console.log(id)
 
-  let link = `https://quizizes.vercel.app/test/${id}`
+  let link = `https://quizees-1.onrender.com/test/${id}`
 
   const handleShareClick = () => {
     navigator.clipboard.writeText(link);
